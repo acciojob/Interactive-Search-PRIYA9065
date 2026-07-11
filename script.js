5810f602e322111caab2fe1ba59c282fac162c83
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2026-07-11 15:09:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //your JS code here. If required.
 const input = document.getElementById("input");
 const btn = document.getElementById("btn");
+const div = document.querySelector(".search");
 
 
 btn.addEventListener("click", () => {
 	input.focus();
+	div.classList.add(active);
+	
 })

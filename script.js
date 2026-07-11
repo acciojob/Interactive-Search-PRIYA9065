010b59691983f5729340465d83940629ac502c80
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2026-07-11 15:02:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const input = document.getElementById("input");
+const btn = document.getElementById("btn");
+
+
+btn.addEventListener("click", () => {
+	input.focus();
+})
